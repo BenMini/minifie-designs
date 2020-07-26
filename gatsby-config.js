4b7c3b799bd9c5f93b644e98533f9d77371bc10e
Ben Minifie <benrs6mini@gmail.com>
2020-07-26 13:44:10 -0400

    Initial Commit"

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Ben Minifie",
+    locale: "en",
+    title: "Ben Minifie",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        basePath: "/",
+        contentPath: "content/",
+        showThemeLogo: false,
+        theme: "classic",
+        icon: "content/images/design.svg",
+      },
+    },
+  ],
+};
